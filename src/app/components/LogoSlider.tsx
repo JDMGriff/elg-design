e43ba41d39418ee2c2f0a44db74bc424837ab674
JDMGriff <griffithsimg@gmail.com>
2025-03-11 09:30:22 +0000

    Responsive Updates
    Carousels updated to be cleaner with responsive

--- src/app/components/LogoSlider.tsx
+++ src/app/components/LogoSlider.tsx
@@ -21,6 +21,9 @@ const LogoSlider = ({ homeData }: LogoSliderProps) => {
                 1280: {
                     perView: 3,
                 },
+                1024: {
+                    perView: 2,
+                },
                 768: {
                     perView: 1,
                 },
@@ -36,12 +39,12 @@ const LogoSlider = ({ homeData }: LogoSliderProps) => {
 
     return (
         <div className="container mx-auto px-6 flex flex-col justify-between md:flex-row items-center border-t border-b border-white py-12 md:py-2 md:px-4">
-            <div className="flex flex-col md:flex-row items-center w-full md:w-[80%]">
-                <div className="w-full md:w-[15%] text-center md:text-left text-white font-semibold">
+            <div className="flex flex-col md:flex-row items-center w-full md:w-[65%] xl:w-[80%]">
+                <div className="w-full md:w-[30%] xl:w-[15%] text-center md:text-left text-white font-semibold">
                     Trusted by the best
                 </div>
 
-                <div className="glide w-full md:!w-[85%]">
+                <div className="glide w-full md:!w-[70%] xl:!w-[85%]">
                     <div className="glide__track" data-glide-el="track">
                         <ul className="glide__slides">
                             {homeData.logo_slider.logos.map((logo) => (
@@ -50,7 +53,7 @@ const LogoSlider = ({ homeData }: LogoSliderProps) => {
                                     className="glide__slide flex items-center justify-center"
                                 >
                                     <Image
-                                        src={`${process.env.NEXT_PUBLIC_BASE_URL}${logo.url}`} // Strapi URL prefix
+                                        src={`${process.env.NEXT_PUBLIC_BASE_URL}${logo.url}`}
                                         alt={`Logo ${logo.documentId}`}
                                         width={150}
                                         height={150}
@@ -61,7 +64,8 @@ const LogoSlider = ({ homeData }: LogoSliderProps) => {
                     </div>
                 </div>
             </div>
-            <div className="w-full md:w-[18%]">
+
+            <div className="w-full md:w-[28%] xl:w-[18%]">
                 <TestimonialCarousel homeData={homeData} />
             </div>
         </div>
--- src/app/components/TestimonialCarousel.tsx
+++ src/app/components/TestimonialCarousel.tsx
@@ -23,9 +23,25 @@ const TestimonialCarousel = ({ homeData }: testimonialProps) => {
     const goNext = () => setIndex((prev) => (prev + 1) % testimonials.length);
 
     return (
-        <div className="flex items-center justify-center w-full">
-            <button className="text-white" onClick={goPrev}>
-                ←
+        <div className="group flex items-center justify-center w-full relative">
+            <button
+                className="text-white opacity-0 group-hover:opacity-100 absolute left-0 transition-all duration-300 drop-shadow"
+                onClick={goPrev}
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="26.871"
+                    height="26.871"
+                    viewBox="0 0 26.871 26.871"
+                >
+                    <path
+                        id="Union_54"
+                        data-name="Union 54"
+                        d="M.029,16.789V2.557A2.2,2.2,0,0,1,.666.628,2.185,2.185,0,0,1,2.221-.02a2.208,2.208,0,0,1,.356.029h14.23a2.192,2.192,0,0,1,0,4.385H4.414V16.789a2.192,2.192,0,0,1-4.385,0Z"
+                        transform="translate(0.014 13.449) rotate(-45)"
+                        fill="#fff"
+                    />
+                </svg>
             </button>
             <div className="text-center">
                 {testimonials.map((item, i) => (
@@ -42,8 +58,24 @@ const TestimonialCarousel = ({ homeData }: testimonialProps) => {
                     </div>
                 ))}
             </div>
-            <button className="text-white" onClick={goNext}>
-                →
+            <button
+                className="text-white opacity-0 group-hover:opacity-100 absolute right-0 transition-all duration-300 drop-shadow"
+                onClick={goNext}
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="26.871"
+                    height="26.871"
+                    viewBox="0 0 26.871 26.871"
+                >
+                    <path
+                        id="Union_54"
+                        data-name="Union 54"
+                        d="M.029,16.789V2.557A2.2,2.2,0,0,1,.666.628,2.185,2.185,0,0,1,2.221-.02a2.208,2.208,0,0,1,.356.029h14.23a2.192,2.192,0,0,1,0,4.385H4.414V16.789a2.192,2.192,0,0,1-4.385,0Z"
+                        transform="translate(26.856 13.422) rotate(135)"
+                        fill="#fff"
+                    />
+                </svg>
             </button>
         </div>
     );
