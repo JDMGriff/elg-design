603351343267c92cc47d057b00948281f6447ce6
JDMGriff <griffithsimg@gmail.com>
2025-03-11 13:49:29 +0000

    routes updated

--- src/app/api/homepage/route.ts
+++ src/app/api/homepage/route.ts
@@ -33,7 +33,7 @@ export async function GET() {
     }
   `;
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}graphql`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}graphql`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
